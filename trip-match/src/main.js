b6ff99f242f7079867c901ed04960cbff43aec1e
ALAYA1803 <rodrigoalaya_2004@hotmail.com>
2025-07-10 09:32:20 -0500

    Fix: main.js:

--- trip-match/src/main.js
+++ trip-match/src/main.js
@@ -1,42 +1,38 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura';
-import i18n from "@/i18n.js";
-import router from '@/router.js';
-import '@fontsource/montserrat';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
-import SelectButton from "primevue/selectbutton";
-import MultiSelect  from 'primevue/multiselect';
-import FileUpload from 'primevue/fileupload';
-import InputNumber from 'primevue/inputnumber';
-import Toast from 'primevue/toast';
-import InputText from 'primevue/inputtext';
-import Button    from 'primevue/button';
-import Select from 'primevue/select';
-import ConfirmationService from 'primevue/confirmationservice';
-import ToastService from 'primevue/toastservice';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from '@/router.js'
+import i18n   from '@/i18n.js'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+import PrimeVue from 'primevue/config'
+import Aura     from '@primeuix/themes/aura'
+import ConfirmationService from 'primevue/confirmationservice'
+import ToastService        from 'primevue/toastservice'
+import Button              from 'primevue/button'
+import InputText           from 'primevue/inputtext'
+import SelectButton        from 'primevue/selectbutton'
+import MultiSelect         from 'primevue/multiselect'
+import FileUpload          from 'primevue/fileupload'
+import InputNumber         from 'primevue/inputnumber'
+import Toast               from 'primevue/toast'
 
-const app = createApp(App);
+const app = createApp(App)
+
 app.use(PrimeVue, {
-    theme: {
-        preset: Aura,
-        options: {}
-    }
-});
+    theme: { preset: Aura },
+    ripple: true
+})
+app.use(ConfirmationService)
+app.use(ToastService)
 
-app.component('pv-input-text', InputText);
-app.component('pv-button',    Button);
-app.component('pv-select-button', SelectButton);
-app.component('pv-multi-select', MultiSelect);
-app.component('pv-file-upload', FileUpload);
-app.component('pv-input-number', InputNumber);
-app.component('Toast', Toast);
-app.component('pv-input-select', Select);
-app.use(ConfirmationService);
-app.use(ToastService);
+app.component('pv-button',        Button)
+app.component('pv-input-text',    InputText)
+app.component('pv-select-button', SelectButton)
+app.component('pv-multi-select',  MultiSelect)
+app.component('pv-file-upload',   FileUpload)
+app.component('pv-input-number',  InputNumber)
+app.component('pv-toast',         Toast)
 
-app.use(i18n);
-app.use(router);
-app.mount('#app');
+app.use(i18n)
+app.use(router)
+app.mount('#app')
